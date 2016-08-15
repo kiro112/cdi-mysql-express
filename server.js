edea1d864ce24d33e2ee0657098058db8e977c79
Jett Calleja <jettcalleja@gmail.com>
2016-08-15 15:02:51 +0800

    install lodash path morgan

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ function start() {
     winston.log('info', 'Starting', config.APP_NAME, 'on', config.ENV, 'environment');
     
     winston.log('verbose', 'Binding 3rd-party middlewares');
+    app.use(require('morgan')('combined', {stream: util.get_log_stream(config.LOGS_DIR)}));
     app.use(require('helmet'));
     app.use(require('method-override')());
     app.use(body_parser.urlencoded({extended: false}));
