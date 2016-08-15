a22f24eb1d4e6c783b2def0c04c8dcbde920f482
Jett Calleja <jettcalleja@gmail.com>
2016-08-15 15:09:24 +0800

    add config file

---
+++ config/config.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const _  = require('lodash');
+const path = require('path');
+
+const config = {
+    APP_NAME: 'CDI App Name',
+
+    FRONTEND_URL: 'cdi.loc',
+
+    PORT: 8000,
+
+    SALT: 'NaCl',
+
+    use: (env) => {
+        _.assign(config, require(__dirname + '/env/' + env));
+        return config;
+    },
+
+};
+
+if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = 'development';
+}
+
+module.exports = config.use(process.env.NODE_ENV);
