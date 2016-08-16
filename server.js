b7028dd52b6ad84bdc7318ca6d3a22454f502794
Jett Calleja <jettcalleja@gmail.com>
2016-08-17 04:10:43 +0800

    fix helmet package

--- server.js
+++ server.js
@@ -44,12 +44,13 @@ function start() {
     
     winston.log('verbose', 'Binding 3rd-party middlewares');
     app.use(require('morgan')('combined', {stream: util.get_log_stream(config.LOGS_DIR)}));
-    app.use(require('helmet'));
+    app.use(require('helmet')());
     app.use(require('method-override')());
     app.use(body_parser.urlencoded({extended: false}));
     app.use(body_parser.json());
     app.use(require('compression')());
-
+    
+    
     winston.log('verbose', 'Binding custom middlewares');
     app.use(require('anytv-node-cors')(config.CORS));
     app.use(require(__dirname + '/lib/res_extended')());
