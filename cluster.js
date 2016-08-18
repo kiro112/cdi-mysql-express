ac53a1c8c7d9350e1d1bc95615d4895497102271
Jett Calleja <jettcalleja@gmail.com>
2016-08-18 09:38:01 +0800

    clean code

--- cluster.js
+++ cluster.js
@@ -1,38 +1,10 @@
 const cluster  = require('cluster');
 const cpuCount = require('os').cpus().length;
 const winston  = require('winston').cli();
+const server   = 'server.js';
 
-let i = cpuCount;
-
-// Listen for dying workers
-cluster.on('exit', function(worker, code, signal) {
-    let cpu      = worker.cpu_number;
-
-    console.log('Worker ' + worker.process.pid + ' died with cpu_number: ' + cpu);
-    console.log('Starting a new worker');
-    cluster.fork({cpu_number: cpu}).on('online', () => {
-
-        let workers  = cluster.workers,
-        keys         = Object.keys(workers);
-        // Get the last element of cluster workers 
-        // and assign its cpu number
-        workers[ keys[ keys.length - 1 ] ].cpu_number = cpu;
-
-    });
-});
 
 if (cluster.isMaster) {
-
-    try {
-        if (!process.argv[2]) {
-            winston.error('Usage: node cluster.js server.js');
-            process.exit();
-        }
-       require.resolve(__dirname + '/' + process.argv[2]);
-    } catch (e) {
-        winston.error('Script to server is missing');
-        process.exit();
-    }
 
     // Fork all the machine's cpus
     for (i = 0; i < cpuCount; i++) {
@@ -46,6 +18,23 @@ if (cluster.isMaster) {
     });
     
 } else {
-    require(__dirname + '/' + process.argv[2]);
+    require(__dirname + '/' + server);
 } 
 
+// Listen for dying workers
+cluster.on('exit', function(worker, code, signal) {
+    let cpu      = worker.cpu_number;
+
+    console.log('Worker ' + worker.process.pid + ' died with cpu_number: ' + cpu);
+    console.log('Starting a new worker');
+    cluster.fork({cpu_number: cpu}).on('online', () => {
+
+        let workers  = cluster.workers,
+        keys         = Object.keys(workers);
+        
+        // Get the last element of cluster workers 
+        // and assign its cpu number
+        workers[ keys[ keys.length - 1 ] ].cpu_number = cpu;
+
+    });
+});
