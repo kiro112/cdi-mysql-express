3d669e873a4faef3bee24832bf575a2378571c46
Jett Calleja <jettcalleja@gmail.com>
2016-08-20 02:08:39 +0800

    fix bugs

--- test/controllers/auth.js
+++ test/controllers/auth.js
@@ -3,10 +3,10 @@ process.env.NODE_ENV = 'development';
 
 const should  = require('chai').should();
 const request = require('supertest');
-const server  = require(__dirname+ '/../server').app;
+const server  = require(__dirname+ '/../../server').app;
 const api     = request.agent(server);
 
-describe('Authentication Login' () => {
+describe('Authentication Login', () => {
 
     let userId,
         token;
@@ -20,10 +20,10 @@ describe('Authentication Login' () => {
                 should.exist(res);
                 res.body.should.have.property('success', false);
                 res.body.should.have.property('errors');
-                res.body.errors.should.have.property('context', 'Invalid username');
+                res.body.errors[0].should.have.property('context', 'Invalid username');
                 done();
            });
     });
 
-    
+
 });
